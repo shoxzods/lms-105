777371a7bf95881495208bac1c1daf35ece17562
shoxzods <ranaldu456@example.com>
2026-07-31 10:28:25 +0500

    feat: add class validators to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './core/database/prisma.module';
 import { SeederModule } from './core/seed/seeder.module';
+import { AuthModule } from './modules/auth/auth.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}) , PrismaModule , SeederModule]
+  imports: [ConfigModule.forRoot({isGlobal:true}) , PrismaModule , SeederModule, AuthModule]
 })
 export class AppModule {}
+ 
