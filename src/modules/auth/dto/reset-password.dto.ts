81a700c7a79a50cc1c3ead5f958fe05844da5014
shoxzods <ranaldu456@gmail.com>
2026-09-03 21:35:29 -0100

    feate:

--- src/modules/auth/dto/reset-password.dto.ts
+++ src/modules/auth/dto/reset-password.dto.ts
@@ -5,7 +5,7 @@ import { IsString, Length, Matches } from "class-validator";
 export class ResetPasswordDto {
   @ApiProperty({ example: "+998901234599" })
   @Matches(/^\+998(77|33|90|91|93|94|95|97|98|99|88|50|55)\d{7}$/, {
-    message: "Telefon raqami noto'g'ri formatda", 
+    message: "Telefon raqami noto'g'ri formatda",
   })
   phone!: string;
 
